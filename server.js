fc7a5fcc7a9371dd0c7984a59507b334b70d3ab2
Gul <gulhassanabd@gmail.com>
2024-08-26 00:24:46 -0700

    port changed to 3000

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('./database/connection');
 const cors = require('cors')
 app.use(cors())
 require('dotenv').config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 var bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded
